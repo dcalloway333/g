398040ead589b2dab023beed3b2c1c804b69b5b4
Paul Calloway <20073761@H2WHK19QQ6NW.tld>
2022-08-30 11:42:01 -0500

    overhaul

--- let.py
+++ let.py
@@ -1,12 +1,82 @@
 import os
 from os import system
+from re import S
 system("clear")
+
 import random
 
-bank = {'texture': 'how something feels', 'accurate': 'on the mark', 'banana': 'a yellow fruit', 'batman':'a viglinte that dresses as a bat', 'cowboys':'dallas based football team', 'halloween':'a holiday in which people dress up in costumes', 'export':'not inport but', 'python': 'coding language names after snake', 'collar':'something around a dogs neck', 'celtics': 'boston based basektball team', 'warriors':'won the 2022 nba finals', 'falcons':'atlanta based football team', 'cheifs':'kansas based football team', 'yankees': 'new york based baseaball team', 'bulls':'chicago based basektball team', 'pistons':'detroit based basektball team', 'rangers':'texas based baseball team', 'rams':'los angeles based football team', 'bucks':'milwaluke based basektball team', 'cubs':'chicago based baseball team', 'nets':'brooklyn based basketball team', 'rockets':'houston based basektball team', 'rays':'tampa bay based baseball team', 'ravens':'baltimore based football team' , 'braves':'atlanta based baseball team', 'mets':'new york based baseball team', 'stars':'dallas based hockey team', 'panthers':'carolina based football team', 'saints':'new orleans based football team', 'texans':'houston based football team', 'nationals':'washington d.c based baseball team'}
-ran_value =random.choice(list(bank.items()))
-ran_key =random.choice(list(bank.keys()))
-print(ran_value)
-print("Your word has", len(ran_key), "letters")
+words = ['texture', 'accurate', 'banana', 'terminal', 'identification', 'substitute', 'favourite', 'revolution', 'elaborate', 'consensus', 'monarch', 'genuine', 'essential', 'wallflower', 'supercilious', 'splendiferous', 'bamboozle', 'cornucopia', 'demitasse', 'doppelganger', 'ersatz', 'festooned', 'gizmo', 'heyday', 'interrobang', 'jalopy', 'juggernaut', 'lummox', 'mnemonic', 'razzmatazz']
 
-# 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
+hint = ['how something feels', 'on the mark', 'a yellow fruit', 'a word for a computer', 'A card to tell who are you', 'a person or thing acting or serving in place of another', 'preferred before all others of the same kind.', 'a forcible overthrow of a government or social order, in favor of a new system.', 'involving many carefully arranged parts or details; detailed and complicated in design and planning.', 'a general agreement.', 'a sovereign head of state, especially a king, queen, or emperor.', 'truly what something is said to be; authentic.', 'absolutely necessary; extremely important.', 'a person who has no one to dance with or who feels shy, awkward, excluded at a party or a flower', 'behaving or looking as though one thinks one is superior to others', 'splendid', 'fool or cheat', "an ornamental container shaped like a goat's horn", 'a small coffee cup', 'an apparition or double of a living person.', 'not real or genuine', 'a place with ribbons, garlands, or other decorations', 'a gadget', "the period of a person's or thing's greatest success, popularity, or vigor", 'a non-standard punctuation mark indicating a question expressed in an exclamatory manner', 'an old car in a dilapidated condition', 'a huge, powerful, and overwhelming force or institution', 'a clumsy, stupid person', 'aiding or designed to aid the memory', 'another term for razzle-dazzle']
+
+dash = []
+
+l = ['Try again!', 'Take another stab!', 'Give it another shot!', 'Keep trying!', 'One more time!']
+lose = random.choice(l)
+
+w = ['Congratulations!', 'Nice Job!', 'Good Work!', 'Fantastic!', 'Well Done!']
+win = random.choice(w)
+
+
+randWord = random.choice(words)
+chances = len(randWord) + 3
+
+
+
+
+word = [*randWord]
+
+
+for num in randWord:
+    dash.append('-')
+
+h = words.index(randWord)
+print('Hint - ' + hint[h])
+
+print(' '.join(dash))
+# print(word)
+# print(chances)
+
+
+x = 1
+low = chances - 1
+
+print('You got ' + str(low) + ' chances left in this game')
+
+while x < chances:
+    x = x + 1
+    low -= 1
+    if '-' in dash:
+        letter = input('Please type a letter not a number: ')
+        if letter in word:
+            for num in range(len(word)):
+                system('clear')
+                if word[num] == letter:
+                    dash[num] = letter
+            print('Hint - ' + hint[h])
+            print('The letter ' + letter + ' was in the word!')
+            print(' '.join(dash))
+            print('You have ' + str(low) + ' chances left')
+       
+       
+        else:
+            system('clear')
+            print('Hint - ' + hint[h])
+            print('The letter '+letter+' was not in the word.')
+            print(' '.join(dash))
+            print('You have ' + str(low) + ' chances left')
+   
+   
+    else:
+        break
+
+
+print('The word was ' + randWord)
+if x == chances and '-' in dash:
+    print('Sorry YOU LOSE.')
+    print(''.join(lose))
+
+
+else:
+    print('You Jest WON THE GAME!')
+    print(''.join(win))
